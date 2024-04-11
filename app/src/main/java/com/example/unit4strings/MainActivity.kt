6014d5391a9caee9b17510a94993a99a96cb1bf0
Cassi <cassi2394@gmail.com>
2024-04-11 12:01:48 -0600

    declare variables

--- app/src/main/java/com/example/unit4strings/MainActivity.kt
+++ app/src/main/java/com/example/unit4strings/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.unit4strings
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var addButton: Button
+    private lateinit var compareButton: Button
+    private lateinit var vowelCountButton: Button
+
+    private lateinit var resultView: TextView
+    private lateinit var string1: EditText
+    private lateinit var string2: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
